9156709f0a658e46244f5d65a07935ee5bc19138
Kazi Mehedi Hasan <hassan.mehedee92@gmail.com>
2024-10-02 13:01:29 +0600

    animation on scroll added on homepage

--- src/pages/Homepage.tsx
+++ src/pages/Homepage.tsx
@@ -10,6 +10,7 @@ import WhyChooseUsCard from '../components/WhyChooseUsCard';
 import Coupon from '../components/ui/Coupon';
 import ContactUs from '../components/ui/ContactUs';
 import ProductCard from '../components/ProductCard';
+import '../css/animationOnScroll.css';
 
 export default function HomePage() {
     const { data: bikeData, isLoading } = useGetBikesQuery({});
@@ -21,9 +22,9 @@ export default function HomePage() {
             <Navbar />
             {/* // hero section starts */}
             <div className="font-pop bg-gray-200">
-                <section className="relative bg-cover bg-center h-screen flex items-center justify-center shadow-xl" style={{ backgroundImage: `url(${homepageCover})` }}>
+                <section className='hero relative bg-cover bg-center h-screen flex items-center justify-center shadow-xl' style={{ backgroundImage: `url(${homepageCover})` }}>
                     <div className="bg-slate-100 opacity-15 w-full h-full absolute top-0 left-0"></div>
-                    <div className="text-center text-white p-10 lg:p-20 bg-dark backdrop-blur-md rounded-lg bg-opacity-40">
+                    <div className="popup text-center text-white p-10 lg:p-20 bg-dark backdrop-blur-md rounded-lg bg-opacity-40">
                         <h1 className="text-4xl font-bold mb-4">Best Bikes for You</h1>
                         <p className="text-xl mb-8">Find your perfect ride from our wide selection of top brands.</p>
                         <input
@@ -36,7 +37,7 @@ export default function HomePage() {
                 </section>
                 {/* // hero section ends */}
                 {/* // featured section starts */}
-                <section className="container mx-auto px-8 mt-12 flex flex-col items-center gap-8">
+                <section className="element container mx-auto px-8 mt-12 flex flex-col items-center gap-8">
                     <h1 className="text-3xl font-bold text-gray-600 text-center mb-6">Ride The Hottest Bikes Now</h1>
                     <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-6">
                         {isLoading && (<Loader />)}
@@ -59,7 +60,7 @@ export default function HomePage() {
                 </section>
                 {/* // featured section ends */}
                 {/* testimonials section starts */}
-                <section className="container px-8 mt-12 mx-auto">
+                <section className="element container px-8 mt-12 mx-auto">
                     <h1 className="text-3xl font-bold text-gray-600 text-center mb-6">Customer Testimonials</h1>
                     <div className="grid grid-cols-1 md:grid-cols-2 justify-items-center lg:grid-cols-3 gap-6">
                         {testimonials.map((testimonial, index) => (
@@ -69,7 +70,7 @@ export default function HomePage() {
                 </section>
                 {/* testimonials section ends */}
                 {/* why choose us section starts */}
-                <section className="container px-8 py-12 mx-auto">
+                <section className=" element container px-8 py-12 mx-auto">
                     <h2 className=" text-3xl font-bold text-gray-600 text-center mb-6">Why Choose Us?</h2>
                     <div className="grid grid-cols-1 md:grid-cols-2 justify-items-center lg:grid-cols-3 gap-6">
                         {
@@ -84,12 +85,12 @@ export default function HomePage() {
                 </section>
                 {/* why choose us section ends */}
                 {/* Coupons and discounts starts */}
-                <section className="text-white">
+                <section className=" element text-white">
                     <Coupon />
                 </section>
                 {/* Coupons and discounts ends */}
                 {/* contact us section starts */}
-                <section className="bg-gray-200">
+                <section className="element bg-gray-200">
                     <ContactUs />
                 </section>
                 {/* contact us section ends */}
